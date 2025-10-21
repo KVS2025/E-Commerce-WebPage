c0b9eda767c10811d8c1fb0e30bec639e7d9a9b1
KVS2025 <kevalshah734@gmail.com>
2025-10-21 11:17:29 +0530

    Modified some jsx files

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import { TrackingPage } from './pages/TrackingPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
+import { TrackingPage } from './pages/tracking/TrackingPage';
 import './App.css'
 
 function App() {
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
